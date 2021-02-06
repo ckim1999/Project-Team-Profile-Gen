13ea09902c80845be8fe4eeebff283b7b399f527
Christopher Kim <chrisnkim1999@gmail.com>
2021-02-06 16:02:59 -0500

    added function for my user's input

--- index.js
+++ index.js
@@ -6,5 +6,30 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // DATA =============================================
+// a list of instructions for the user to answer accordingly
+
+const userInput = [
+    {
+        type = "input",
+        name = "team_man_name",
+        message = "Input your Team Manager's name: "
+    },
+    {
+        type = "input",
+        name = "employeeID",
+        message = "Input your employee ID: "
+    },
+    {
+        type = "input",
+        name = "email",
+        message = "Input your email address: "
+    },
+    {
+        type = "input",
+        name = "office_num",
+        message = "Input your office number: "
+    },    
+]
+
 // FUNCTIONS ========================================
 // USER INTERACTIONS ================================
